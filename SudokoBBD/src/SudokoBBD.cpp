856ba5f945f7263db22c93d2438b78877938c18f
Stefan Ekman <stekman@stekman-hp.(none)>
2013-04-09 21:24:25 +0200

    Default implementation

---
+++ SudokoBBD/src/SudokoBBD.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SudokoBBD.cpp
+// Author      : Stefan Ekman
+// Version     :
+// Copyright   : Free to use, copy and abuse in any way you like
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
